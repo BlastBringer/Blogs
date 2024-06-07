da5419f1485ccda86b9afe3c5d8ec3ffd6a72f97
unknown <rahulsaravanan126@gmail.com>
2024-06-07 14:26:50 +0530

    some dev in blog

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
-document.getElementById('darkModeToggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const leftPillar = document.querySelector('.pillar.left');
+    const rightPillar = document.querySelector('.pillar.right');
+  
+    function createSpiral() {
+      const spiral = document.createElement('div');
+      spiral.classList.add('spiral');
+      return spiral;
+    }
+  
+    for (let i = 0; i < 10; i++) {
+      leftPillar.appendChild(createSpiral());
+      rightPillar.appendChild(createSpiral());
+    }
+  });
+  
